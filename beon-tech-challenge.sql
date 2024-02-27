53984d968a8c39261158da5eb64cf8c75a7d0046
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2024-02-27 12:22:42 -0300

    feat: query created during beon tech interview

---
+++ beon-tech-challenge.sql
@@ -0,0 +1,40 @@
+-- Question 1
+WITH distinct_dest_ewr_count AS (
+SELECT COUNT(DISTINCT dest) as qty
+FROM `beontech.tb_flights` A
+WHERE origin = "EWR"
+)
+-- Question 1 variation
+, distinct_dest_ewr_list AS (
+SELECT DISTINCT dest
+FROM `beontech.tb_flights` A
+WHERE origin = "EWR"
+)
+-- Question 2
+, avg_dest_per_day AS (
+  SELECT month, ROUND(AVG(qty),2) FROM (
+  SELECT month, day, COUNT(DISTINCT dest) qty
+  FROM `beontech.tb_flights`
+  GROUP BY 1, 2)
+  GROUP BY 1
+)
+-- Question 7
+, rank_airlines AS (
+  SELECT B.name AS airline, COUNT(A.flight) as qty_flights
+  FROM `beontech.tb_flights` A
+  LEFT JOIN `beontech.tb_airlines` B
+  USING(carrier)
+  GROUP BY 1 ORDER BY 2 DESC
+)
+-- Extra question
+, cumulative_sum AS (
+  SELECT DISTINCT DATE(year, month, day) AS date
+  , SUM(dep_delay) OVER (ORDER BY DATE(year, month, day) ASC)
+  FROM `beontech.tb_flights`
+)
+SELECT * FROM cumulative_sum;
+
+-- Extra question: View to use in Looker Studio
+CREATE OR REPLACE VIEW beontech.vw_flight AS 
+SELECT DATE(year, month, day) AS flight_date, * FROM `beontech.tb_flights`;
+
